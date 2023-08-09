c64dce5d69243ffd2bb5eedf03ee28e3c2b2aa2d
Darthpool9900 <89270284+Darthpool9900@users.noreply.github.com>
2023-08-09 18:18:07 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Card from './components/card'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="w-full h-screen bg-gray-950 flex items-center justify-center">
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/card.tsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <>
+      <div  className=" bg-gray-500 text-xl flex items-center justify-between">
+       
+      </div>
+    </>
+    )
+}
+
+export default Card;
